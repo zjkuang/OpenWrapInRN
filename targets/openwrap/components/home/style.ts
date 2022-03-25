5e34acc88210cffbb47dd87c0de974451ec3ef0f
Zhengqian Kuang <dev.kuang@gmail.com>
2022-03-24 21:00:05 -0600

    debugging...

--- targets/openwrap/components/home/style.ts
+++ targets/openwrap/components/home/style.ts
@@ -44,7 +44,7 @@ export const styles = StyleSheet.create({
   },
   bottomAd: {
     backgroundColor: 'cyan',
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
 });
